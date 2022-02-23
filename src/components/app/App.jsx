bd761509caef98b0c46e08f270308e4910537400
JosieMCantu <josiex2@gmail.com>
2022-02-23 12:53:21 -0800

    functional but need refactoring

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,63 +1,56 @@
 import React, { useState } from 'react';
-
-// record - function to record a new current value
-// undo - function that sets the current value to the previous value
-// redo - function that sets the current value forward in history
-// current - the current value
+import styles from '../app/App.css';
 
 const useRecord = () => {
-
 };
 
 function App() {
-  const [current, setCurrent] = useState('#FF0000');
-  const [undo, setUndo] = useState('');
-  const [redo, setRedo] = useState('');
+  // const { current, undo, redo, record } = useRecord('#FF0000');
+  const [current, setCurrent] = useState('');
+  const [index, setIndex] = useState(-1);
   const [record, setRecord] = useState([]);
 
-  console.log('current', current);
-  console.log('undo', undo);
-  console.log('redo', redo);
-  console.log('record', record);
-  // const { current, undo, redo, record } = useRecord('#FF0000');
+  console.log('CURRENT', current);
+  console.log('RECORD', record);
+  console.log('INDEX', index);
 
-  const handleClick = ({ target }) => {
-    
+  const handleColorChange = ({ target }) => {
+    const newColor = record.splice([index + 1], 0, target.value);
     setCurrent(target.value);
-    setRecord(record.concat(current));
+    setIndex(index + 1);
   }
 
+  const handleUndoClick = () => {
+    
+    setCurrent(record[index - 1]);
+    setIndex(index - 1);
+  }
+
+  const handleRedoClick = () => {
+    setIndex(index + 1);
+    setCurrent(record[index + 1]);
+  }
+ 
   return (
     <>
-
-      <button onClick={undo}>undo</button>
-      <button onClick={redo}>redo</button>
-      <input type='color' value={current} onChange={handleClick} />
-
-      <div style={{ background: current, width: '10rem', height: '10rem' }}></div>
+      <div className={styles.buttons}>
+        <button disabled={index === -1} onClick={handleUndoClick}>undo</button>
+        <button  disabled={index === record.length - 1} onClick={handleRedoClick}>redo</button>
+        <input type='color' value={current} onChange={handleColorChange} />
+      </div>
+      
+      <div style={{ 
+        marginLeft: '50px',
+        marginTop: '10px',
+        background: current, 
+        width: '10rem', 
+        height: '10rem',
+        border: '3px solid black',
+        borderRadius: '5px'}}></div>
     </>
   )
 }
 
 export default App;
 
-// Behavior
 
-// select a color (e.g red), see the square change (to red)
-// select another color (e.g blue), see the square change (to blue)
-// select another color (e.g green), see the square change (to green)
-
-// press undo, see the square change to the second color (blue) 
-// press undo, see the square change to the first color (red)
-
-// press redo, see the square change to the second color (blue)
-// select another color (e.g yellow), see the square change (to yellow)
-
-// press undo, see the square change to the second color (blue)
-// press undo, see the square change to the first color (red)
-
-// press redo, see the square change to the second color (blue)
-// press redo, see the square change to the fourth color (yellow)
-// press redo, see the square change to the third color (green)
-
-// Write a test to test behavior
